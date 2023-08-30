e9dd8b6812b279dd62438d10d7198c3e1e12c3c6
Martin Frigaard <mjfrigaard@pm.me>
2023-08-29 22:53:38 -0700

    updates to app.qmd

--- _common.R
+++ _common.R
@@ -68,13 +68,26 @@ co_box <- function(color, header, contents = "Your text", fold = FALSE, look = "
   )
 }
 
-git_margin_box <- function(fig_pw = '70%', branch = 'main', repo = 'shinyap') {
-  cat(paste0(
+git_margin_box <- function(contents = "standard", fig_pw = '70%', branch = 'main', repo = 'shinyap') {
+  switch(contents,
+  standard = cat(paste0(
       "\n",
       "::: {.column-margin}\n\n",
       "![Git Branch](img/new_branch_ico.png){width='", fig_pw, "'}\n\n",
       "This section's code is in the [`", repo, "`](https://github.com/mjfrigaard/", repo, ")
       repo's [`", branch, "`](https://github.com/mjfrigaard/", repo, "/tree/", branch, ") branch \n\n",
       "::: \n\n"
-    ))
+    )),
+  alt = cat(paste0(
+      "\n",
+      ":::: {.column-margin}\n\n",
+      "::: {style='font-weight: bold; font-size: 1.00em; color: #6F1AB6;'}\n\n",
+      "![Git Branch](img/new_branch_ico.png){width='", fig_pw, "'}\n\n",
+      "[`", repo, "`](https://github.com/mjfrigaard/", repo, ")
+      repo's [`", branch, "`](https://github.com/mjfrigaard/", repo, "/tree/", branch, ") branch \n\n",
+      "::: \n\n",
+      "::::", "\n"
+    )),
+    stop("Invalid `type`", call. = FALSE)
+  )
 }
