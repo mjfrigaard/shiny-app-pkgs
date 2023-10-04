233ab4d309c4ffd84a3771624c1713337a97d84c
Martin Frigaard <mjfrigaard@pm.me>
2023-10-04 07:34:41 -0700

    updates to shinytest2 section

--- _common.R
+++ _common.R
@@ -78,17 +78,17 @@ git_margin_box <- function(contents = "standard", fig_pw = '70%', branch = 'main
   standard = cat(paste0(
       "\n",
       "::: {.column-margin}\n\n",
-      "![Git Branch](img/new_branch_ico.png){width='", fig_pw, "'}\n\n",
-      "This section's code is in the [`", repo, "`](https://github.com/mjfrigaard/", repo, ")
-      repo's [`", branch, "`](https://github.com/mjfrigaard/", repo, "/tree/", branch, ") branch \n\n",
+      "![](img/new_branch_ico.png){width='", fig_pw, "' fig-align='center'}\n\n",
+      "\nThis section's code is in the [`", branch, "`](https://github.com/mjfrigaard/", repo, "/tree/", branch, ")
+      branch of [`", repo, "`](https://github.com/mjfrigaard/", repo, ").\n\n",
       "::: \n\n"
     )),
   alt = cat(paste0(
       "\n",
       ":::: {.column-margin}\n\n",
       "::: {style='font-weight: bold; font-size: 1.00em; color: #6F1AB6;'}\n\n",
-      "![Git Branch](img/new_branch_ico.png){width='", fig_pw, "'}\n\n",
-      "[`", repo, "`](https://github.com/mjfrigaard/", repo, ")
+      "![](img/new_branch_ico.png){width='", fig_pw, "' fig-align='center'}\n\n",
+      "\n[`", repo, "`](https://github.com/mjfrigaard/", repo, ")
       repo's [`", branch, "`](https://github.com/mjfrigaard/", repo, "/tree/", branch, ") branch \n\n",
       "::: \n\n",
       "::::", "\n"
